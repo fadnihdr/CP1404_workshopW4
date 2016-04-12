b301604b2ec1bf125925f2bb32c8880a7866e518
fadnihdr <fadnihdr@gmail.com>
2016-04-12 15:02:29 +0800

    concept ascii_table

--- Ascii_table.py
+++ Ascii_table.py
@@ -1,7 +1,22 @@
-lower = 10
-upper = 100
-lower_input = int(input("Enter a number ({}-{}):".format(lower,upper)))
-upper_input = int(input("Enter a number ({}-{}):".format(lower,upper)))
 
-for i in range(lower_input,upper_input):
-    print("{} {}".format(i,chr(i))strip())
+def get_number(lower=10,upper=50):
+    user_input = input("Enter Enter a number {}-{}".format(lower, upper))
+    while True:
+        if user_input.isdecimal() == True:
+            if int(user_input) >= lower and int(user_input) <= upper:
+                for i in range(int(user_input), 120, 11):
+                    print("{} {}").format(i,chr(i)).strip()
+                break
+            else:
+                    print("Enter a valid number!")
+                    user_input = input("Enter Enter a number {}-{}".format(lower, upper))
+        else:
+            print("Enter a valid number!")
+            user_input = input("Enter Enter a number {}-{}".format(lower, upper))
+
+
+get_number()
+
+
+
+
