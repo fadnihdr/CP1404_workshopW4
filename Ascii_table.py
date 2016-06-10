c88d039c10a0db7df8f161530633d1223118fc2f
fadnihdr <fadnihdr@gmail.com>
2016-06-10 21:14:08 +0800

    fix

--- Ascii_table.py
+++ Ascii_table.py
@@ -1,11 +1,12 @@
 
-def get_number(lower=10,upper=50):
+def get_number(lower=10, upper=50):
     user_input = input("Enter Enter a number {}-{}".format(lower, upper))
+    user_input2 = input("Enter Enter a number {}-{}".format(lower, upper))
     while True:
-        if user_input.isdecimal() == True:
-            if int(user_input) >= lower and int(user_input) <= upper:
-                for i in range(int(user_input), 120, 11):
-                    print("{} {}").format(i,chr(i)).strip()
+        if user_input.isdecimal() == True and user_input2.isdecimal() == True:
+            if int(user_input) >= lower and int(user_input) <= upper and int(user_input2) >= lower and int(user_input2) <= upper:
+                for i in range(int(user_input),int(user_input2)):
+                    print("{} {}".format(i, chr(i)).strip())
                 break
             else:
                     print("Enter a valid number!")
